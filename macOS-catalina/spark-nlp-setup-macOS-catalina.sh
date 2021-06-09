8d54cf2842241709bfc2ff709910e3dd0dd8f210
Duc Ha <ducha@Ducs-MacBook-Pro.local>
2021-06-09 23:20:46 +0200

    update for macOS catalina

--- macOS-catalina/spark-nlp-setup-macOS-catalina.sh
+++ macOS-catalina/spark-nlp-setup-macOS-catalina.sh
@@ -1,58 +1,57 @@
-#!/bin/sh
-# if there are any problems, refer to
-# https://medium.com/spark-nlp/introduction-to-spark-nlp-installation-and-getting-started-part-ii-d009f7a177f3
+#!/bin/bash
 
-echo "Starting Spark NLP Installation script."
+PYSPARK="3.0.2"
+SPARKNLP=$PUBLIC_VERSION
+SPARKHOME="$PWD/spark-3.1.1-bin-hadoop2.7"
 
-echo "\nChecking for brew"
-which -s brew
-if [[ $? != 0 ]] ; then
-    echo "\nPlease install homebrew, the package manager for macOS: https://brew.sh/"
-else
-    echo "brew update"
-    brew update
+echo "Setup PySpark $PYSPARK and Spark NLP $SPARKNLP and Spark OCR $OCR_VERSION and Spark NLP for Healthcare $JSL_VERSION"
+
+JAVA_VER=$(java -version 2>&1 | sed -n ';s/.* version "\(.*\)\.\(.*\)\..*"/\1\2/p;')
+
+if [[ $JAVA_VER -ne "18" ]]; then
+  echo "Please install openjdk-8. Example with brew https://formulae.brew.sh/formula/openjdk@8"
 fi
 
+echo "Downloading Spark $PYSPARK"
+if [[ "$PYSPARK" == "3.1"* ]]; then
+  if [[ ! -e "spark-3.1.1-bin-hadoop2.7.tgz" ]]; then
+    curl "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" --output spark-3.1.1-bin-hadoop2.7.tgz
+    tar -xf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
+  fi
+  SPARKHOME="$PWD/spark-3.1.1-bin-hadoop2.7"
+elif [[ "$PYSPARK" == "3.0"* ]]; then
+  if [[ ! -e "spark-3.0.2-bin-hadoop2.7.tgz" ]]; then
+    curl "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" --output spark-3.0.2-bin-hadoop2.7.tgz
+    tar -xf spark-3.0.2-bin-hadoop2.7.tgz > /dev/null
+  fi
+  SPARKHOME="$PWD/spark-3.0.2-bin-hadoop2.7"
+elif [[ "$PYSPARK" == "2"* ]]; then
+  if [[ ! -e "spark-2.4.7-bin-hadoop2.7.tgz" ]]; then
+    curl "https://downloads.apache.org/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz" --output spark-2.4.7-bin-hadoop2.7.tgz
+    tar -xf spark-2.4.7-bin-hadoop2.7.tgz > /dev/null
+  fi
+  SPARKHOME="$PWD/spark-2.4.7-bin-hadoop2.7"
+else
+  if [[ ! -e "spark-3.1.1-bin-hadoop2.7.tgz" ]]; then
+    curl "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" --output spark-3.1.1-bin-hadoop2.7.tgz
+    tar -xf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
+  fi
+  SPARKHOME="$PWD/spark-3.1.1-bin-hadoop2.7"
+fi
 
-install_openjdk() {
-    echo "\nInstalling openjdk@8"
-    brew install openjdk@8
-    echo "Creating symlink /usr/local/opt/openjdk@8/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-8.jdk"
-    sudo ln -sfn /usr/local/opt/openjdk@8/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-8.jdk
-    echo "Please add this line to your shell configuration (~/.zshrc):"
-    echo "export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-8.jdk/Contents/Home"
-}
+export SPARK_HOME=$SPARKHOME
 
-echo "\nDo you wish to install openjdk-8?
-This will create a link in /Library/Java/JavaVirtualMachines/openjdk-8.jdk"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) install_openjdk; break;;
-        No ) echo "\nPlease make sure openjdk-8 is installed and JAVA_HOME is set."; break;;
-    esac
-done
+# Install pyspark spark-nlp
+! pip3 install -U pip 
+! pip3 install --upgrade pyspark==$PYSPARK spark-nlp==$SPARKNLP findspark
+! pip3 install --upgrade spark-nlp-jsl==$JSL_VERSION  --extra-index-url https://pypi.johnsnowlabs.com/$SECRET
+! pip3 install --upgrade scikit-image pillow==8.1.2 implicits
+! pip3 install https://pypi.johnsnowlabs.com/$JSL_OCR_SECRET/spark-ocr/spark-ocr-$OCR_VERSION.tar.gz
 
-install_py_venv() {
-    echo "Installing in Virtual environment"
-    pip3 install virtualenv notebook
-    python3 -m virtualenv ./spark-nlp-venv
-    source ./spark-nlp-venv/bin/activate
-    pip3 install numpy pandas pyspark==3.1.1 spark-nlp==3.0.3 ipykernel
-    python -m ipykernel install --user --name spark-nlp --display-name "Spark NLP"
-    deactivate
-    echo "\nInstallation done! You can now start using Spark NLP in your Jupyter Notebook, by selecting the \"Spark NLP\" kernel."
-}
-
-install_py_local() {
-    echo "Installing in local Python"
-    pip3 install notebook pandas numpy pyspark==3.1.1 spark-nlp==3.0.3
-    echo "\nInstallation done! You can now start using Spark NLP in your Jupyter Notebook."
-}
-
-echo "\nFor which Python would you like to install Spark NLP?"
-select yn in "Local" "Virtual Environment"; do
-    case $yn in
-        "Local") install_py_local; break;;
-        "Virtual Environment") install_py_venv; break;;
-    esac
-done
+# NLP Jar
+#curl https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/jars/spark-nlp-assembly-$SPARKNLP.jar
+## NLP Healthcare Jar
+curl https://pypi.johnsnowlabs.com/$SECRET/spark-nlp-jsl-$JSL_VERSION.jar --output spark-nlp-jsl-$JSL_VERSION.jar
+## OCR Jar
+OCR_VERSION=3.2.0-spark30
+curl https://pypi.johnsnowlabs.com/$JSL_OCR_SECRET/jars/spark-ocr-assembly-$OCR_VERSION.jar --output spark-ocr-assembly-$OCR_VERSION.jar
