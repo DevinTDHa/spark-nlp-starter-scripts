618d30b3aa31537f1741c22636f226370c37f60b
Trung Duc Ha <duc.hatrung95@gmail.com>
2021-06-18 18:48:36 +0200

    docker update

--- Docker-x86_64/build_scripts/install_packages.sh
+++ Docker-x86_64/build_scripts/install_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /build_scripts/SPARKNLP_VERSIONS
 pip3 install --no-cache-dir --upgrade --extra-index-url https://pypi.johnsnowlabs.com/$SECRET spark-nlp-jsl==$JSL_VERSION
-pip3 install --no-cache-dir --upgrade spark-nlp==$PUBLIC_VERSION implicits
+pip3 install --no-cache-dir --upgrade spark-nlp==$PUBLIC_VERSION
 pip3 install --no-cache-dir --upgrade --no-deps https://pypi.johnsnowlabs.com/$JSL_OCR_SECRET/spark-ocr/spark-ocr-$OCR_BASE_VERSION.$OCR_SPARK_VERSION.tar.gz
 
 mkdir -p jars
